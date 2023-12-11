f3bb38577caa7749fcd47e018b1f80cee6b8744b
jbalingbingcpe <143565219+jbalingbingcpe@users.noreply.github.com>
2023-12-11 14:23:31 +0800

    Update emtech2.py

--- emtech2.py
+++ emtech2.py
@@ -1,30 +1,62 @@
-
 import streamlit as st
 from tensorflow import keras
-import os
+from keras.preprocessing import image as keras_image
+from keras.preprocessing.image import img_to_array
 import numpy as np
-from PIL import Image, ImageOps
-from tensorflow.keras.preprocessing.image import img_to_array
-from keras.models import load_model
+import os
 
+# Get the absolute path to the current script's directory
+script_directory = os.path.dirname(os.path.abspath(__file__))
+
+# Specify the file name
+model_file_path = os.path.join(script_directory, 'model_checkpoint.h5')
 
 # Load the saved model
-best_model = keras.models.load_model('model_checkpoint.h5')
+best_model = keras.models.load_model(model_file_path)
 
 # Function to preprocess an image for prediction
 def preprocess_image(img):
+    img = img.resize((128, 128))  # Resize the image to match the model's expected input shape
     img_array = img_to_array(img)
     img_array = np.expand_dims(img_array, axis=0)
     img_array /= 255.0  # Normalize the image
 
     return img_array
 
-def predict(image):
-    # Preprocess the image
-    preprocessed_image = preprocess_image(image)
+def create_custom_model(input_shape):
+    model = keras.models.Sequential()
+    model.add(keras.layers.InputLayer(input_shape=input_shape))  # Input layer
 
-    # Make predictions
-    predictions = best_model.predict(preprocessed_image)
+    # Iterate through layers in the original model
+    for layer in best_model.layers[1:]:
+        if isinstance(layer, keras.layers.Conv2D):
+            # Create a new convolutional layer with the same parameters
+            new_conv = keras.layers.Conv2D(layer.filters, layer.kernel_size, activation=layer.activation, input_shape=input_shape)
+            model.add(new_conv)
+        elif isinstance(layer, keras.layers.MaxPooling2D):
+            model.add(layer)
+        elif isinstance(layer, keras.layers.Flatten):
+            model.add(layer)
+        elif isinstance(layer, keras.layers.Dense):
+            # Create a new dense layer with the same parameters
+            new_dense = keras.layers.Dense(layer.units, activation=layer.activation)
+            model.add(new_dense)
+
+    # Add a new dense layer with the desired output shape
+    model.add(keras.layers.Dense(1, activation='sigmoid', name='custom_dense'))
+
+    return model
+
+def predict(img):
+    # Preprocess the image
+    preprocessed_image = preprocess_image(img)
+
+    # Create a custom model with the desired architecture
+    input_shape = preprocessed_image.shape[1:]
+    custom_model = create_custom_model(input_shape)
+
+    # Make predictions using the custom model
+    predictions = custom_model.predict(preprocessed_image)
 
     # Get the predicted class (0 for cat, 1 for dog, assuming 2-class classification)
     predicted_class = np.argmax(predictions)
@@ -32,17 +64,17 @@ def predict(image):
     return "Cat" if predicted_class == 0 else "Dog"
 
 def main():
-    st.title("Cat or Dog Predictor")
+    st.title("IS IT A CAT OR DOG?")
 
     uploaded_file = st.file_uploader("Choose a file")
 
     if uploaded_file is not None:
-        image = image.load_img(uploaded_file, target_size=(128, 128))
-        st.image(image, caption="Uploaded Image", use_column_width=True)
+        img = keras_image.load_img(uploaded_file, target_size=(128, 128))
+        st.image(img, caption="Uploaded Image", use_column_width=True)
 
-        prediction = predict(image)
-
-        st.write(f"Prediction: {prediction}")
+        if st.button("PREDICT"):
+            prediction = predict(img)
+            st.write(f"PREDICTION: {prediction}")
 
 if __name__ == "__main__":
     main()
